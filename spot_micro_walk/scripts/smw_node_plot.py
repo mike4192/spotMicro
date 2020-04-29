3dfa00f49bca0189a2ba12fa8aefa9d90beb3063
mike <1541992+mike4192@users.noreply.github.com>
2020-04-29 12:48:06 -0400

    spot micro walk plotting node for test/simulation

---
+++ spot_micro_walk/scripts/smw_node_plot.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+
+from spot_micro_walk.run_robot_plot import main
+
+# Node program, calls main function in spot_micro_simple_command python package
+if __name__ == '__main__':
+    main()
