a0f8f47a44c033ebcdbca1e461a9dd7b4f3fbb61
mike <1541992+mike4192@users.noreply.github.com>
2020-06-03 13:21:52 -0400

    lcd monitor displays angle and speed commands and state string

--- lcd_monitor/src/lcd_monitor/sm_lcd_driver.py
+++ lcd_monitor/src/lcd_monitor/sm_lcd_driver.py
@@ -3,6 +3,7 @@ from lcd_monitor import I2C_LCD_driver
 import datetime
 import time
 import rospy
+from math import pi
 from geometry_msgs.msg import Vector3
 from std_msgs.msg import String
 
@@ -14,6 +15,7 @@ class SpotMicroLcd():
 		self._mylcd = I2C_LCD_driver.lcd()
 
 		self._state_str = 'None'
+		self._padded_state_str = 'None'
 
 		self._fwd_speed_cmd = 0.0
 		self._side_speed_cmd = 0.0
@@ -31,31 +33,46 @@ class SpotMicroLcd():
 
 	def update_speed_cmd(self, msg):
 		''' Updates speed command attributes'''
-		self._fwd_speed_cmd = msg.x
-		self._side_speed_cmd = msg.z
-		self._yaw_rate_cmd = msg.y
+		self._fwd_speed_cmd = msg.x*100.0
+		self._side_speed_cmd = msg.y*100.0
+		self._yaw_rate_cmd = msg.z*180.0/pi
 
 	def update_angle_cmd(self, msg):
 		''' Updates angle command attributes'''
-		self._phi_cmd = msg.x
-		self._theta_cmd = msg.y
-		self._psi_cmd = msg.z
+		self._phi_cmd = msg.x * 180.0/pi
+		self._theta_cmd = msg.y * 180.0/pi
+		self._psi_cmd = msg.z * 180.0/pi
 
 	def update_state_string(self, msg):
 		''' Updates angle command attributes'''
 		self._state_str = msg.data
 
+		if self._state_str == "Transit Stand":
+			self._padded_state_str = "To Stand"
+		elif self._state_str == "Transit Idle":
+			self._padded_state_str = "To Idle"
+		else:
+			self._padded_state_str = self._state_str
+		
+		self._padded_state_str = self._padded_state_str.ljust(16,' ')
+
 	def run(self):
 		''' Runs the lcd driver and prints data'''
 
 		# Define the loop rate in Hz
-		rate = rospy.Rate(1)
+		rate = rospy.Rate(3)
 
 		while not rospy.is_shutdown():
+			
+			self._mylcd.lcd_display_string('State: %s'%(self._padded_state_str),1)
 
-			self._mylcd.lcd_display_string('State: %s'%(self._state_str),1)
-			# mylcd.lcd_display_string("%2i:%2i:%2i"%(hour,minute,second), 2)
 
+			if self._state_str == "Stand":
+				self._mylcd.lcd_display_string('x%3.0f y%3.0f z%3.0f'%(self._phi_cmd, self._theta_cmd, self._psi_cmd),2)
+			elif self._state_str == "Walk":
+				self._mylcd.lcd_display_string('x%3.0f y%3.0f z%3.0f'%(self._fwd_speed_cmd, self._side_speed_cmd, self._yaw_rate_cmd),2)
+			else:
+				self._mylcd.lcd_display_string('                ',2)
 			# Sleep till next loop
 			rate.sleep()
 
