a2709ab7920e6ee45167af1b6bb383f6e1f0387a
mike <1541992+mike4192@users.noreply.github.com>
2020-06-03 12:27:41 -0400

    fixes to lcd monitor code

--- lcd_monitor/src/lcd_monitor/sm_lcd_driver.py
+++ lcd_monitor/src/lcd_monitor/sm_lcd_driver.py
@@ -6,32 +6,6 @@ import rospy
 from geometry_msgs.msg import Vector3
 from std_msgs.msg import String
 
-# if __name__ == "__main__":
-# 	mylcd = I2C_LCD_driver.lcd()
-
-# 	rospy.init_node('lcd_monitor_node')
-
-#         rospy.Subscriber('sm_speed_cmd',Float32,self.update_x_speed_cmd)
-#         rospy.Subscriber('sm_angle_cmd',Float32,self.update_x_speed_cmd)
-#         rospy.Subscriber('sm_state',Float32,self.update_x_speed_cmd)
-# 	rate = rospy.Rate(1)	
-
-# 	while not rospy.is_shutdown():
-# 		hour = datetime.datetime.now().hour
-# 		minute = datetime.datetime.now().minute
-# 		second = datetime.datetime.now().second
-# 		#mylcd.lcd_clear()
-
-# 		# Subscribe to state string message, velocity command, angle command
-# 		# If in stand state display angle commands
-# 		# If in walk state, display rate commands
-# 		# State: {Idle, To Stand, Stand, To Idle, Walk}
-# 		# x1.2 y1.2 z1.2 For speed stuff
-
-# 		mylcd.lcd_display_string('Hi Maggie!! :)',1)
-# 		mylcd.lcd_display_string("%2i:%2i:%2i"%(hour,minute,second), 2)
-# 		rate.sleep()
-
 class SpotMicroLcd():
 	''' Class to encapsulate lcd driver for spot micro robot '''
 
@@ -52,8 +26,8 @@ class SpotMicroLcd():
 		rospy.init_node('lcd_monitor_node')
 
 		rospy.Subscriber('sm_speed_cmd',Vector3,self.update_speed_cmd)
-        rospy.Subscriber('sm_angle_cmd',Vector3,self.update_angle_cmd)
-        rospy.Subscriber('sm_state',String,self.update_state_string)
+        	rospy.Subscriber('sm_angle_cmd',Vector3,self.update_angle_cmd)
+        	rospy.Subscriber('sm_state',String,self.update_state_string)
 
 	def update_speed_cmd(self, msg):
 		''' Updates speed command attributes'''
@@ -73,7 +47,7 @@ class SpotMicroLcd():
 
 	def run(self):
 		''' Runs the lcd driver and prints data'''
-		mylcd.lcd_display_string('State: %s'%(self._state_str),1)
+		self._mylcd.lcd_display_string('State: %s'%(self._state_str),1)
 		# mylcd.lcd_display_string("%2i:%2i:%2i"%(hour,minute,second), 2)
 
 
@@ -84,4 +58,4 @@ class SpotMicroLcd():
 
 def main():
 	sm_lcd_obj = SpotMicroLcd()
-	sm_lcd.run()
+	sm_lcd_obj.run()
