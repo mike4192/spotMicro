8133f927c042b300bdf105681952efa6f78571ed
Sascha Bias <sb@liztv.net>
2021-03-06 16:35:30 +0100

    some refactoring

--- spot_micro_joy/scripts/spotMicroJoystickMove.py
+++ spot_micro_joy/scripts/spotMicroJoystickMove.py
@@ -9,15 +9,15 @@ from math import pi
 
 
 class SpotMicroJoystickControl():
-    BUTTON_GREEN = 0
-    BUTTON_RED = 1
-    BUTTON_YELLOW = 2
-    BUTTON_BLUE = 3
+    BUTTON_IDLE = 0
+    BUTTON_WALK = 1
+    BUTTON_STAND = 2
+    BUTTON_ANGLE = 3
 
-    AXES_ROLL = 0
-    AXES_HEIGHT = 1
-    AXES_YAW = 2
-    AXES_PITCH = 3
+    ANGLE_AXES_ROLL = 0
+    ANGLE_AXES_HEIGHT = 1
+    ANGLE_AXES_YAW = 2
+    ANGLE_AXES_PITCH = 3
 
     WALK_AXES_FORWARD = 1
     WALK_AXES_STRAFE = 0
@@ -28,17 +28,20 @@ class SpotMicroJoystickControl():
     MODE_ANGLE = 2
     MODE_WALK = 3
 
+    MAX_ROLL_DEG = 45
+    MAX_YAW_DEG = 45
+    MAX_PATCH_DEG = 45
+
+    MAX_FORWARD_SPEED = 0.05
+    MAX_STRAFE_SPEED = 0.05
+    MAX_YAW_SPEED_DEG = 15
+
     def __init__(self):
 
         self._angle_cmd_msg = Vector3()
         self._angle_cmd_msg.x = 0
         self._angle_cmd_msg.y = 0
         self._angle_cmd_msg.z = 0
-
-        self._speed_cmd_msg = Vector3()
-        self._speed_cmd_msg.x = 0
-        self._speed_cmd_msg.y = 0
-        self._speed_cmd_msg.z = 0
 
         self._vel_cmd_msg = Twist()
         self._vel_cmd_msg.linear.x = 0
@@ -93,45 +96,51 @@ class SpotMicroJoystickControl():
         self._ros_pub_angle_cmd.publish(self._angle_cmd_msg)
 
     def on_joy(self, msg):
-        if msg.buttons[self.BUTTON_GREEN] == 1:
+        self.on_joy_buttons(msg.buttons)
+        self.on_joy_axes(msg.axes)
+
+    def on_joy_buttons(self, buttons):
+        if buttons[self.BUTTON_IDLE] == 1:
             self._ros_pub_idle_cmd.publish(self._idle_event_cmd_msg)
             rospy.loginfo('Idle command issued from joystick.')
             self.mode = self.MODE_IDLE
-
-        if msg.buttons[self.BUTTON_YELLOW] == 1:
+        elif buttons[self.BUTTON_STAND] == 1:
             self._ros_pub_stand_cmd.publish(self._stand_event_cmd_msg)
             rospy.loginfo('Stand command issued from joystick.')
             self.mode = self.MODE_STAND
-
-        if msg.buttons[self.BUTTON_BLUE] == 1:
+        elif buttons[self.BUTTON_ANGLE] == 1:
             self.reset_all_angle_commands_to_zero()
             rospy.loginfo('Entering joystick angle command mode.')
             self.mode = self.MODE_ANGLE
-
-        if msg.buttons[self.BUTTON_RED] == 1:
+        elif buttons[self.BUTTON_WALK] == 1:
             self.reset_all_angle_commands_to_zero()
             self._ros_pub_walk_cmd.publish(self._walk_event_cmd_msg)
             rospy.loginfo('Entering joystick walk command mode.')
             self.mode = self.MODE_WALK
 
+    def on_joy_axes(self, axes):
         if self.mode == self.MODE_ANGLE:
-            print('Cmd Values: phi: %1.3f deg, theta: %1.3f deg, psi: %1.3f deg ' \
-                  % (
-                  self._angle_cmd_msg.x * 180 / pi, self._angle_cmd_msg.y * 180 / pi, self._angle_cmd_msg.z * 180 / pi))
+            self.on_joy_angle_mode(axes)
+        elif self.mode == self.MODE_WALK:
+            self.on_joy_walk_mode(axes)
 
-            self._angle_cmd_msg.x = pi / 180 * msg.axes[self.AXES_ROLL] * 45 * -1
-            self._angle_cmd_msg.y = pi / 180 * msg.axes[self.AXES_PITCH] * 45 * -1
-            self._angle_cmd_msg.z = pi / 180 * msg.axes[self.AXES_YAW] * 45
-            self._ros_pub_angle_cmd.publish(self._angle_cmd_msg)
+    def on_joy_walk_mode(self, axes):
+        self._vel_cmd_msg.linear.x = axes[self.WALK_AXES_FORWARD] * self.MAX_FORWARD_SPEED
+        self._vel_cmd_msg.linear.y = axes[self.WALK_AXES_STRAFE] * self.MAX_STRAFE_SPEED
+        self._vel_cmd_msg.angular.z = pi / 180 * axes[self.WALK_AXES_YAW] * self.MAX_YAW_SPEED_DEG
+        print('Cmd Values: x speed: %1.3f m/s, y speed: %1.3f m/s, yaw rate: %1.3f deg/s ' \
+              % (self._vel_cmd_msg.linear.x, self._vel_cmd_msg.linear.y, self._vel_cmd_msg.angular.z * 180 / pi))
+        self._ros_pub_vel_cmd.publish(self._vel_cmd_msg)
 
-        if self.mode == self.MODE_WALK:
-            self._vel_cmd_msg.linear.x = msg.axes[self.WALK_AXES_FORWARD] * 0.05
-            self._vel_cmd_msg.linear.y = msg.axes[self.WALK_AXES_STRAFE] * 0.05
-            self._vel_cmd_msg.angular.z = pi / 180 * msg.axes[self.AXES_YAW] * 15
-
-            self._ros_pub_vel_cmd.publish(self._vel_cmd_msg)
-            print('Cmd Values: x speed: %1.3f m/s, y speed: %1.3f m/s, yaw rate: %1.3f deg/s ' \
-                  % (self._vel_cmd_msg.linear.x, self._vel_cmd_msg.linear.y, self._vel_cmd_msg.angular.z * 180 / pi))
+    def on_joy_angle_mode(self, axes):
+        self._angle_cmd_msg.x = pi / 180 * axes[self.ANGLE_AXES_ROLL] * self.MAX_ROLL_DEG * -1
+        self._angle_cmd_msg.y = pi / 180 * axes[self.ANGLE_AXES_PITCH] * self.MAX_PATCH_DEG * -1
+        self._angle_cmd_msg.z = pi / 180 * axes[self.ANGLE_AXES_YAW] * self.MAX_YAW_DEG
+        print('Cmd Values: phi: %1.3f deg, theta: %1.3f deg, psi: %1.3f deg ' \
+              % (
+                  self._angle_cmd_msg.x * 180 / pi, self._angle_cmd_msg.y * 180 / pi,
+                  self._angle_cmd_msg.z * 180 / pi))
+        self._ros_pub_angle_cmd.publish(self._angle_cmd_msg)
 
     def run(self):
         print("green = idle")
